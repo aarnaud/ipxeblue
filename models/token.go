08d09cb6d1f38b4f37a211de468c15303124aae3
Anthony ARNAUD <github@anthony-arnaud.fr>
2021-01-07 20:45:12 -0500

    delete cascade for token

--- models/token.go
+++ models/token.go
@@ -7,13 +7,12 @@ import (
 
 type Token struct {
 	Token         string    `gorm:"primaryKey" json:"token"`
-	Computer      Computer  `gorm:"ForeignKey:ComputerUUID;References:Uuid,Name" json:"-"`
+	Computer      Computer  `gorm:"ForeignKey:ComputerUUID;References:Uuid;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"-"`
 	ComputerUUID  uuid.UUID `gorm:"not null" json:"computer_uuid"`
-	Bootentry     Bootentry `gorm:"ForeignKey:BootentryUUID;References:Uuid,Name" json:"-"`
+	Bootentry     Bootentry `gorm:"ForeignKey:BootentryUUID;References:Uuid,Name;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"-"`
 	BootentryUUID uuid.UUID `gorm:"not null" json:"bootentry_uuid"`
 	// BootentryFile can be null if we generate DownloadBaseURL
-	BootentryFile     *BootentryFile `gorm:"ForeignKey:BootentryUUID,Filename;References:BootentryUUID,Name" json:"-"`
-	BootentryfileUUID *uuid.UUID     `json:"-"`
-	Filename          *string        `json:"filename"`
-	ExpireAt          time.Time      `json:"expire"`
+	BootentryFile *BootentryFile `gorm:"ForeignKey:BootentryUUID,Filename;References:BootentryUUID,Name;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"-"`
+	Filename      *string        `json:"filename"`
+	ExpireAt      time.Time      `json:"expire"`
 }
